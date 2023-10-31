2def6afc1d4393c0440000304108c633e29b6b5e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-31 13:29:15 -0400

    ts vers

--- main.ts
+++ main.ts
@@ -2,7 +2,48 @@
  *
  * Created by: Liya G
  * Created on: Oct 2023
- * This program ...
+ * This program counts down from 4 to 0 and lights up neopixels.
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+let sprite: game.LedSprite = null
+let loopCounter: number = 4
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Skull)
+pause(500)
+basic.clearScreen()
+
+// neopixel setup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+
+// button A pressed
+input.onButtonPressed(Button.A, function () {
+  // setup
+  basic.clearScreen()
+  let loopCounter = 4
+
+  // neopixel
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.show()
+
+  // loop
+  while (loopCounter >= 0) {
+    basic.pause(500)
+    basic.showNumber(loopCounter)
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    loopCounter = loopCounter - 1
+  }
+})
